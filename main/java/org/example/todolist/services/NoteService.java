b5af60afa889d47dc6a8a65b5a9f96506da67fe6
SspidersS <99414766+SspidersS@users.noreply.github.com>
2025-02-23 11:56:48 +0100

    Add files via upload

---
+++ main/java/org/example/todolist/services/NoteService.java
@@ -0,0 +1,55 @@
+package org.example.todolist.services;
+
+import org.example.todolist.model.Note;
+
+import java.util.*;
+
+public class NoteService {
+    private Map<Long, Note> noteStorage = new HashMap<>();
+    private static Random random = new Random();
+
+    public List<Note> listAll() {
+        return new ArrayList<>(noteStorage.values());
+    }
+
+    public Note add(Note note) {
+        long id = generateUniqueId();
+        note.setId(id);
+        noteStorage.put(id, note);
+        return note;
+    }
+
+    public void deleteById(long id) {
+        if (noteStorage.containsKey(id)) {
+            noteStorage.remove(id);
+        } else {
+            throw new NoSuchElementException("Note with id " + id + " not found.");
+        }
+    }
+
+    public void update(Note note) {
+        if (noteStorage.containsKey(note.getId())) {
+            Note existingNote = noteStorage.get(note.getId());
+            existingNote.setTitle(note.getTitle());
+            existingNote.setContent(note.getContent());
+        } else {
+            throw new NoSuchElementException("Note with id " + note.getId() + " not found.");
+        }
+    }
+
+    public Note getById(long id) {
+        if (noteStorage.containsKey(id)) {
+            return noteStorage.get(id);
+        } else {
+            throw new NoSuchElementException("Note with id " + id + " not found.");
+        }
+    }
+
+    private long generateUniqueId() {
+        long id;
+        do {
+            id = Math.abs(random.nextLong());
+        } while (noteStorage.containsKey(id));
+        return id;
+    }
+}
